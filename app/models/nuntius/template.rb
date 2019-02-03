55a77339316fca3fb798bf2b4067b198b9bce5f4
Tom de Grunt <tom@degrunt.nl>
2019-02-03 19:41:33 +0100

    Add in template -> message generation

--- app/models/nuntius/template.rb
+++ app/models/nuntius/template.rb
@@ -6,8 +6,22 @@ module Nuntius
       where('metadata->>:name = :value', name: name, value: value)
     }
 
-    def new_message
-      Nuntius::Message.new(template: template, transport: transport)
+    def new_message(assigns)
+      message = Nuntius::Message.new(template: self, transport: transport)
+
+      message.to = render(:to, assigns)
+      message.subject = render(:subject, assigns)
+      message.html = render(:html, assigns, layout: layout&.html)
+      message.text = render(:text, assigns, layout: layout&.text)
+
+      message
+    end
+
+    private
+
+    def render(attr, assigns, options = {})
+      ::Liquor.render(send(attr),
+                    assigns: assigns.merge('template' => self).merge(options))
     end
   end
 end
