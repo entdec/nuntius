d346102f141f98b9c6cb7749c9dd2a4e75733962
Tom de Grunt <tom@degrunt.nl>
2019-02-03 13:26:16 +0100

    Not needed (here)

--- app/transports/nuntius/base_transport.rb
+++ app/transports/nuntius/base_transport.rb
@@ -31,31 +31,5 @@ module Nuntius
     def self.class_from_name(name)
       Nuntius.const_get("#{name}_transport".camelize)
     end
-
-    private
-
-    def with_message_instance(obj)
-      message_instance = message_instance_for(obj)
-      # message_instance.request_id = Praesens.request_id
-      yield message_instance
-      # Dont save the instance if it does not belong to the object
-      message_instance.save if message_instance.persisted?
-    rescue StandardError => e
-      message_instance.state = :failed
-      message_instance.feedback = { type: 'Error', info: e.message }
-      message_instance.save if message_instance.persisted?
-      Rails.logger.error "Message: #{e.message}: #{e.backtrace.join('; ')}"
-    end
-
-    def message_instance_for(obj)
-      base = obj if obj.respond_to?(:message_instances)
-      base ||= obj.shipment if obj.respond_to?(:shipment)
-      base ||= obj.user if obj.respond_to?(:user)
-      return base.message_instances.create(message: self, state: :sending) if base
-
-      # Returning a blank instance if no message instance present, this way we can keep state but we will not save it.
-      MessageInstance.new(message: self)
-    end
-
   end
 end
