94da963eca3ebf600b2853b2765b7f1625cfc9f3
Tom de Grunt <tom@degrunt.nl>
2019-02-03 19:45:59 +0100

    Some test stuff

--- test.rb
+++ test.rb
@@ -3,17 +3,31 @@
 # puts status_mapping
 require 'pp'
 
-m = Nuntius::Message.create(to: '+31641085630', text: "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} - test", transport: 'sms')
+# m = Nuntius::Message.create(to: '+31641085630', text: "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} - test", transport: 'sms')
+# m = Nuntius::Message.create(to: '5c5f8f06e8808bddbd0f7883709a2903124bfe445bcca3807a2997a3480674da', text: "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} - test", transport: 'push')
 
-t = Nuntius::TwilioSmsProvider.new()
+# m = Nuntius::Message.create(to: 'cgLW7RRyqhQ:APA91bHAKexgybHs8ndIFI-OqkB0KjLOJOt_HV3fULF4v2erXT9QiuOstDbf0gHWwgcyKAM1KYuonYdpvokUo2UGVyVxIhoBZRljQZgdSOVmhEdII4zJDgIlPcqvZd_C0NhIJhcxLZOL', text: "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} - test", transport: 'push')
 
-st = Nuntius::SmsTransport.new
+m = Nuntius::Message.create(to: 'tom@degrunt.nl', html: '<b>Hoi</b>',  text: "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} - test", transport: 'mail')
 
-pp st.deliver(m)
+# t = Nuntius::TwilioSmsProvider.new()
+# t = Nuntius::TwilioSmsProvider.new()
+
+st = Nuntius::MailTransport.new
+
+a = Account.first
+Nuntius.with(a).message('created')
+
+# messenger = AccountMessenger.new(a, 'created', {})
+# templates = messenger.call
+# messenger.dispatch(templates) if templates
+# messenger.call
+
+# pp st.deliver(m)
 
 binding.pry
 
-pp m
+pp a
 
 # response = t.send(m)
 #
