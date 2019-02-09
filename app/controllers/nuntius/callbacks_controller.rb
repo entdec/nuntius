740f0e37ffe345c77f1d5e32e09e54fb4ba7453c
Tom de Grunt <tom@degrunt.nl>
2019-02-09 21:57:13 +0100

    Add callbacks controller

---
+++ app/controllers/nuntius/callbacks_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_dependency 'nuntius/application_controller'
+require 'preamble'
+
+module Nuntius
+  class CallbacksController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    layout false
+
+    def create
+      message = Nuntius::Message.find(params[:message_id])
+      response = message.nuntius_provider.callback(message, params)
+
+      render body: response[2].first,
+             content_type: response[1]['Content-Type'],
+             layout: false
+    end
+  end
+end
