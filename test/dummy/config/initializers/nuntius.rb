0a87bd82a71943d15a01c958328cb2f6469f4686
Tom de Grunt <tom@degrunt.nl>
2019-02-09 22:00:04 +0100

    Add voice config

--- test/dummy/config/initializers/nuntius.rb
+++ test/dummy/config/initializers/nuntius.rb
@@ -7,6 +7,7 @@ Nuntius.setup do |config|
   config.transport :mail
   config.transport :push
   config.transport :sms
+  config.transport :voice
 
   config.provider :smtp, transport: :mail, settings: { from_header: '"Local Express" <do_not_reply@localexpress.nl>', host: 'email-smtp.eu-west-1.amazonaws.com', port: 587, username: 'AKIAIIA7EIIM25OFDEUA', password: 'AgJjPlUKEdMuyIuJ2LshcuvSygCXpTEe6VV/bVwEhZV+'}
 
@@ -15,4 +16,6 @@ Nuntius.setup do |config|
 
   config.provider :message_bird, transport: :sms, priority: 1, timeout: 20, settings: { from: 'Soverin', auth_token: 'live_qrV8ZwTAgnqQ6soTZBddZMtjK' }
   config.provider :twilio, transport: :sms, priority: 2, settings: { sid: 'AC92bf1782ac7790aa62d13f2135a887aa', auth_token: '811738b3a314daa224ce55ca400a97c2', from: '+14153736696' }
+
+  config.provider :twilio, transport: :voice, settings: { sid: 'AC92bf1782ac7790aa62d13f2135a887aa', auth_token: '811738b3a314daa224ce55ca400a97c2', from: '+14153736696' }
 end
