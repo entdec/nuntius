ce202c5877b35298ce380fe0919c0c92ef600dc4
Tom de Grunt <tom@degrunt.nl>
2019-03-14 14:10:05 +0100

    Start with maintenance

--- app/controllers/nuntius/admin/messages_controller.rb
+++ app/controllers/nuntius/admin/messages_controller.rb
@@ -1,64 +1,70 @@
 # frozen_string_literal: true
 
-class Admin::MessagesController < ApplicationController
-  include Authenticated
-  before_action :set_objects, except: %i[index create destroy]
-  authorize_resource
+require_dependency 'nuntius/application_admin_controller'
 
-  add_breadcrumb I18n.t('breadcrumbs.admin.messages'), :admin_messages_path
+module Nuntius
+  module Admin
+    class Admin::MessagesController < ApplicationAdminController
+      before_action :set_objects, except: %i[index create destroy]
+      authorize_resource
 
-  def new
-    @message = current_channel.messages.new
-    render :edit
-  end
+      add_breadcrumb I18n.t('breadcrumbs.admin.messages'), :admin_messages_path
 
-  def create
-    @message = current_channel.messages.new(message_params)
-    flash_and_redirect @message.save, admin_messages_url, 'Message created successfully', 'There were problems creating the message'
-  end
+      def new
+        @message = current_channel.messages.new
+        render :edit
+      end
 
-  def index; end
+      def create
+        @message = current_channel.messages.new(message_params)
+        flash_and_redirect @message.save, admin_messages_url, 'Message created successfully', 'There were problems creating the message'
+      end
 
-  def show
-    redirect_to :edit_admin_message
-  end
+      def index;
+      end
 
-  def edit
-    @message = Message.accessible_for(current_user).find(params[:id])
-    add_breadcrumb I18n.t('breadcrumbs.edit'), edit_admin_message_path(@message)
-  end
+      def show
+        redirect_to :edit_admin_message
+      end
 
-  def update
-    @message = Message.accessible_for(current_user).find(params[:id])
-    flash_and_redirect @message.update(message_params), admin_messages_url, 'Message updated successfully', 'There were problems updating the message'
-  end
+      def edit
+        @message = Message.accessible_for(current_user).find(params[:id])
+        add_breadcrumb I18n.t('breadcrumbs.edit'), edit_admin_message_path(@message)
+      end
 
-  def duplicate
-    message = Message.accessible_for(current_user).find(params[:id])
-    @message = message.amoeba_dup
-    render :edit
-  end
+      def update
+        @message = Message.accessible_for(current_user).find(params[:id])
+        flash_and_redirect @message.update(message_params), admin_messages_url, 'Message updated successfully', 'There were problems updating the message'
+      end
 
-  def destroy
-    @message = Message.accessible_for(current_user).find(params[:id])
-    @message.destroy
-    redirect_to admin_messages_url
-  end
+      def duplicate
+        message = Message.accessible_for(current_user).find(params[:id])
+        @message = message.amoeba_dup
+        render :edit
+      end
 
-  private
+      def destroy
+        @message = Message.accessible_for(current_user).find(params[:id])
+        @message.destroy
+        redirect_to admin_messages_url
+      end
 
-  def set_objects
-    @retailers = Retailer.accessible_for(current_user).order(:name)
-    @companies = Company.accessible_for(current_user).order(:name)
-    @layouts = Message.accessible_for(current_user)
-  end
+      private
 
-  def message_params
-    params.require(:message).permit(:channel_id, :conditions, :query, :to, :kind, :klass,
-                                    :event, :timestamp, :interval, :description, :subject,
-                                    :html, :text, :layout_id, :filter, :retailer_id, :company_id, preview_ids: []).tap do |whitelisted|
-      whitelisted[:preview_ids] = [*params[:message][:preview_ids]].reject(&:empty?)
-      whitelisted[:query] = JSON.parse(whitelisted[:query]) if whitelisted[:query]
+      def set_objects
+        @retailers = Retailer.accessible_for(current_user).order(:name)
+        @companies = Company.accessible_for(current_user).order(:name)
+        @layouts = Message.accessible_for(current_user)
+      end
+
+      def message_params
+        params.require(:message).permit(:channel_id, :conditions, :query, :to, :kind, :klass,
+                                        :event, :timestamp, :interval, :description, :subject,
+                                        :html, :text, :layout_id, :filter, :retailer_id, :company_id, preview_ids: []).tap do |whitelisted|
+          whitelisted[:preview_ids] = [*params[:message][:preview_ids]].reject(&:empty?)
+          whitelisted[:query] = JSON.parse(whitelisted[:query]) if whitelisted[:query]
+        end
+      end
     end
   end
 end
--- app/controllers/nuntius/admin/templates_controller.rb
+++ app/controllers/nuntius/admin/templates_controller.rb
@@ -5,13 +5,29 @@ require_dependency 'nuntius/application_admin_controller'
 module Nuntius
   module Admin
     class TemplatesController < ApplicationAdminController
+
+      add_breadcrumb I18n.t('nuntius.breadcrumbs.admin.templates'), :admin_templates_path
+
       def index
         @templates = Template.all
       end
+
       def new
         @template = Template.new
         render :edit
       end
+
+      def create
+        @template = Template.new(template_params)
+        flash_and_redirect @template.save, admin_templates_url, 'Template created successfully', 'There were problems creating the template'
+      end
+
+      private
+
+      def template_params
+        params.require(:template).permit(:klass, :event, :transport, :description, :metadata, :from, :to, :subject, :html, :text, :payload)
+      end
+
     end
   end
 end
