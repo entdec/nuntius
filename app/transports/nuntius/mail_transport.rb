14a0ee599ded1e5a1a65bf0be1bd9dccbdd67f97
Tom de Grunt <tom@degrunt.nl>
2019-03-22 12:17:19 +0100

    Split per to

--- app/transports/nuntius/mail_transport.rb
+++ app/transports/nuntius/mail_transport.rb
@@ -2,5 +2,13 @@
 
 module Nuntius
   class MailTransport < BaseTransport
+    # We split per email address, to allow easy resends
+    def deliver(message)
+      message.to.split(',').each do |to|
+        new_message = message.dup
+        new_message.to = to
+        super(new_message)
+      end
+    end
   end
 end
