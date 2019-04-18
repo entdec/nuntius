01b6d2aa49dc4d8fd37cb5d91e08d75db8086a28
Tom de Grunt <tom@degrunt.nl>
2019-04-18 15:24:21 +0200

    Add nuntius breadcrumb always

--- app/controllers/nuntius/application_controller.rb
+++ app/controllers/nuntius/application_controller.rb
@@ -3,5 +3,6 @@
 module Nuntius
   class ApplicationController < Nuntius.config.base_controller.constantize
     protect_from_forgery with: :exception
+    add_breadcrumb I18n.t('nuntius.breadcrumbs.nuntius'), :root_url
   end
 end
