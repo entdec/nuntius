e925effb7d7d2b85f4d5de1ac530972c37631775
Tom de Grunt <tom@degrunt.nl>
2019-08-25 08:30:28 +0000

    Sleepy?

--- db/migrate/20190825080757_update_nuntius_message_sending_to_sent.rb
+++ db/migrate/20190825080757_update_nuntius_message_sending_to_sent.rb
@@ -1,8 +1,8 @@
 class UpdateNuntiusMessageSendingToSent < ActiveRecord::Migration[5.2]
   def up
-    NuntiusMessage.where(state: 'sending').update_all(state: 'sent')
+    Nuntius::Message.where(state: 'sending').update_all(state: 'sent')
   end
   def down
-    NuntiusMessage.where(state: 'sent').update_all(state: 'sending')
+    Nuntius::Message.where(state: 'sent').update_all(state: 'sending')
   end
 end
