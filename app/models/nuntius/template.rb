ca35a49128cf783a0fe47108f7ec5d067217ffa7
Tom de Grunt <tom@degrunt.nl>
2019-08-28 19:56:51 +0200

    Reorder and add blank_or_in

--- app/models/nuntius/template.rb
+++ app/models/nuntius/template.rb
@@ -17,16 +17,20 @@ module Nuntius
       where('metadata->>:name IS NULL', name: name)
     }
 
-    scope :metadata_blank_or_eql, lambda { |name, value|
-      where('metadata->>:name IS NULL OR metadata->>:name = :value', name: name, value: value)
-    }
-
     scope :metadata_eql, lambda { |name, value|
       where('metadata->>:name = :value', name: name, value: value)
     }
 
+    scope :metadata_blank_or_eql, lambda { |name, value|
+      where('metadata->>:name IS NULL OR metadata->>:name = :value', name: name, value: value)
+    }
+
     scope :metadata_in, lambda { |name, value|
       where('metadata->>:name IN (:value)', name: name, value: value)
+    }
+
+    scope :metadata_blank_or_in, lambda { |name, value|
+      where('metadata->>:name IS NULL OR metadata->>:name IN (:value)', name: name, value: value)
     }
 
     def new_message(object, assigns = {})
