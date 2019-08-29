f560e7779d1ae1f9920ba9bde5fbf49c44e453b5
Andre Meij <andre@itsmeij.com>
2019-08-29 09:54:59 +0200

    Add Devise support

---
+++ app/services/nuntius/initialize_for_class_service.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'query_constructor'
+module Nuntius
+  class InitializeForClassService < ApplicationService
+    attr_reader :klass, :name, :options
+
+    def initialize(klass, options = {})
+      @klass = klass
+      @name = klass.name
+      @options = options
+    end
+
+    def perform
+      raise "Nuntius Messenger missing for class #{name}, please create a #{messenger}" unless messenger
+
+      add_to_config
+      create_events
+      override_devise if options[:override_devise]
+    end
+
+    private
+
+    def override_devise
+      klass.send(:define_method, :send_devise_notification) { |notification, *params| Nuntius.with(self, params).message(notification.to_s) }
+    end
+
+    def add_to_config
+      Nuntius.config.nuntiable_class_names << name unless Nuntius.config.nuntiable_class_names.include?(name)
+    end
+
+    def nuntiable_events
+      events = []
+      events += nuntiable_events_from_state_machine if options[:use_state_machine]
+      events += nuntiable_events_from_devise if options[:override_devise]
+      events
+    end
+
+    def nuntiable_events_from_state_machine
+      if klass.respond_to?(:aasm)
+        klass.aasm.events.map(&:name)
+      elsif klass.respond_to?(:state_paths)
+        klass.state_machine.events.map(&:name)
+      else
+        []
+      end
+    end
+
+    def nuntiable_events_from_devise
+      I18n.t('devise.mailer').keys.map(&:to_s)
+    end
+
+    # add all known events to the messenger class as actions
+    def create_events
+      nuntiable_events.each do |event_name|
+        messenger.send(:define_method, event_name) { |object, params = {}| }
+      end
+    end
+
+    def messenger
+      Nuntius::BaseMessenger.messenger_for_class(name)
+    end
+  end
+end
--- lib/nuntius/active_record_helpers.rb
+++ lib/nuntius/active_record_helpers.rb
@@ -4,29 +4,9 @@ module ActiveRecordHelpers
   extend ActiveSupport::Concern
 
   class_methods do
-    def nuntiable(use_state_machine: false)
+    def nuntiable(options = {})
       has_many :messages, as: :nuntiable, class_name: 'Nuntius::Message'
-
-      Nuntius.config.nuntiable_class_names << name unless Nuntius.config.nuntiable_class_names.include? name
-
-      raise "Nuntius Messenger missing for class #{name}, please create a #{Nuntius::BaseMessenger.messenger_name_for_class(name)}" unless Nuntius::BaseMessenger.messenger_for_class(name)
-
-      return if use_state_machine == false
-
-      messenger = Nuntius::BaseMessenger.messenger_for_class(name)
-
-      nuntiable_events = if respond_to?(:aasm)
-                 aasm.events.map(&:name)
-               elsif respond_to?(:state_paths)
-                 state_machine.events.map(&:name)
-               else
-                 []
-               end
-
-      # add all state-machine events to the messenger class as actions
-      nuntiable_events.each do |name|
-        messenger.send(:define_method, name) { |object, params = {}| }
-      end
+      Nuntius::InitializeForClassService.new(self, options).call
     end
   end
 end
