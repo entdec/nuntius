5f735aacf17c154de3acd750a9171fa189ec1e52
Tom de Grunt <tom@degrunt.nl>
2019-09-20 16:42:10 +0200

    Bugfix, bump

--- app/controllers/nuntius/application_admin_controller.rb
+++ app/controllers/nuntius/application_admin_controller.rb
@@ -4,7 +4,7 @@ require_dependency 'nuntius/application_controller'
 
 module Nuntius
   class ApplicationAdminController < ApplicationController
-    include Concerns::Respond
+    include Nuntius::Concerns::Respond
     include Nuntius.config.admin_authentication_module.constantize if Nuntius.config.admin_authentication_module
   end
 end
