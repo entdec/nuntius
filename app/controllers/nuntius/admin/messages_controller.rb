a1bbaf59dd0d81f7cee30987b8757953e8a092ae
Tom de Grunt <tom@degrunt.nl>
2019-09-21 12:39:36 +0200

    Remove visible for now

--- app/controllers/nuntius/admin/messages_controller.rb
+++ app/controllers/nuntius/admin/messages_controller.rb
@@ -8,14 +8,16 @@ module Nuntius
       add_breadcrumb(I18n.t('nuntius.breadcrumbs.admin.messages'), :admin_messages_path) if defined? add_breadcrumb
 
       def index
-        @messages = Nuntius::Message.visible.order(created_at: :desc)
+        # FIXME: put back visible
+        @messages = Nuntius::Message.order(created_at: :desc)
         if params[:template_id]
           @messages = @messages.where(template_id: params[:template_id])
         end
       end
 
       def show
-        @message = Nuntius::Message.visible.find(params[:id])
+        # FIXME: put back visible
+        @message = Nuntius::Message.find(params[:id])
         add_breadcrumb(@message.id, admin_message_path(@message)) if defined? add_breadcrumb
       end
 
