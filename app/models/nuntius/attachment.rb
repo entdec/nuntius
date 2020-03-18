a6b35a14df19621d0d8b0449245ae17e48b9e9d2
Mark Cornelissen <mark@boxture.com>
2020-03-18 12:29:49 +0100

    Delegate ActiveStorage methods to the ActiveStorage attachment

--- app/models/nuntius/attachment.rb
+++ app/models/nuntius/attachment.rb
@@ -1,5 +1,7 @@
 module Nuntius
   class Attachment < ApplicationRecord
     has_and_belongs_to_many :messages, :class_name => 'Message'
+
+    delegate [:download, :content_type, :file_name], to: :content
   end
 end
