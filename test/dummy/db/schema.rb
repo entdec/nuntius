9137817a1fa9c8b600ee6fe6f7b9747aa2e4741f
Mark Cornelissen <mark@boxture.com>
2020-03-18 12:11:39 +0100

    Updates schema.rb

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_132337) do
+ActiveRecord::Schema.define(version: 2020_03_18_095339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 2020_02_24_132337) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "nuntius_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "nuntius_attachments_messages", id: false, force: :cascade do |t|
+    t.bigint "nuntius_messsages_id"
+    t.bigint "nuntius_attachments_id"
+    t.index ["nuntius_attachments_id"], name: "index_nuntius_attachments_messages_on_nuntius_attachments_id"
+    t.index ["nuntius_messsages_id"], name: "index_nuntius_attachments_messages_on_nuntius_messsages_id"
   end
 
   create_table "nuntius_campaigns", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
