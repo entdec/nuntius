ebe068568aecd862f3c741063841ebec3e8e1ab7
Mark Cornelissen <mark@boxture.com>
2020-03-18 12:38:22 +0100

    Corrected delegate

--- app/models/nuntius/attachment.rb
+++ app/models/nuntius/attachment.rb
@@ -2,6 +2,6 @@ module Nuntius
   class Attachment < ApplicationRecord
     has_and_belongs_to_many :messages, :class_name => 'Message'
 
-    delegate [:download, :content_type, :file_name], to: :content
+    delegate :download, :content_type, :file_name, to: :content
   end
 end
