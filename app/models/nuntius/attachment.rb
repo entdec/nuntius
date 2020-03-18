24f6829947fdcd7a58e086aa16264499d1969b54
Mark Cornelissen <mark@boxture.com>
2020-03-18 14:26:15 +0100

    Pass on signed_id to content

--- app/models/nuntius/attachment.rb
+++ app/models/nuntius/attachment.rb
@@ -2,6 +2,6 @@ module Nuntius
   class Attachment < ApplicationRecord
     has_and_belongs_to_many :messages, :class_name => 'Message'
 
-    delegate :download, :content_type, :filename, to: :content
+    delegate :download, :content_type, :filename, :signed_id, to: :content
   end
 end
