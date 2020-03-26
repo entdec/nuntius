76ac69de7453ed1bc7bb72f57f3b68411ce362ec
Mark Cornelissen <mark@boxture.com>
2020-03-26 10:38:35 +0100

    Test the allow_list function

--- test/models/nuntius/message_test.rb
+++ test/models/nuntius/message_test.rb
@@ -5,8 +5,9 @@ require 'test_helper'
 module Nuntius
   class MessageTest < ActiveSupport::TestCase
     include ActiveJob::TestHelper
+
     test 'delivering a message will persist it and queue it for delivery' do
-      message = nuntius_messages(:one)
+      message = nuntius_messages(:one_recipient)
 
       perform_enqueued_jobs do
         message.deliver_as(:mail)
@@ -23,8 +24,24 @@ module Nuntius
       end
     end
 
+    test 'delivering a message that does not match the allow list will block it' do
+      message = nuntius_messages(:blocked_recipient)
+
+      perform_enqueued_jobs do
+        message.deliver_as(:mail)
+        assert message
+        assert message.persisted?
+        message.reload
+        assert_equal 'blocked', message.status
+        assert_equal 'mail', message.transport
+        assert_equal 'smtp', message.reload.provider
+
+        assert_equal 0, Mail::TestMailer.deliveries.length
+      end
+    end
+
     test 'delivering a mail to two recipients will deliver separate messages' do
-      message = nuntius_messages(:two)
+      message = nuntius_messages(:two_recipients)
 
       perform_enqueued_jobs(only: Nuntius::TransportDeliveryJob) do
         message.deliver_as(:mail)
@@ -36,6 +53,22 @@ module Nuntius
       assert_equal ['test@example.com'], mail.to
       mail = Mail::TestMailer.deliveries.last
       assert_equal ['test2@example.com'], mail.to
+      Mail::TestMailer.deliveries.clear
+    end
+
+    test 'delivering a mail to multiple recipients will deliver separate messages to allowed recipients' do
+      message = nuntius_messages(:blocked_and_non_blocked_recipients)
+
+      perform_enqueued_jobs(only: Nuntius::TransportDeliveryJob) do
+        message.deliver_as(:mail)
+      end
+      assert_performed_jobs 3
+
+      assert_equal 2, Mail::TestMailer.deliveries.length
+      mail = Mail::TestMailer.deliveries.first
+      assert_equal ['mark@boxture.com'], mail.to
+      mail = Mail::TestMailer.deliveries.last
+      assert_equal ['test@example.com'], mail.to
       Mail::TestMailer.deliveries.clear
     end
   end
