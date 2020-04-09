7b407ff61902eac56ce8abf95128d8ae93a55918
Mark Cornelissen <mark@boxture.com>
2020-04-09 10:27:04 +0200

    Test the interval_time_range method

--- test/models/nuntius/template_test.rb
+++ test/models/nuntius/template_test.rb
@@ -24,5 +24,20 @@ module Nuntius
       t = Template.new(interval: '7 days')
       assert_equal 7.days, t.interval_duration
     end
+
+    test 'interval_time_range gives an empty range when there is no interval' do
+      t = Template.new
+      assert_equal 0.seconds..0.seconds, t.interval_time_range
+    end
+
+    test 'interval_time_range for after events' do
+      t = Template.new(interval: '3 hours', event: 'after_the_fact')
+      assert_equal (3.hours.ago..2.hours.ago).inspect, t.interval_time_range.inspect
+    end
+
+    test 'interval_time_range for before events' do
+      t = Template.new(interval: '2 hours', event: 'before_the_fact')
+      assert_equal (2.hours.after..3.hour.after).inspect, t.interval_time_range.inspect
+    end
   end
 end
