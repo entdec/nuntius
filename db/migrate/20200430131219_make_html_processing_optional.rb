82655ba9a1da54181510d71296ae88a8c64c9dae
Andre Meij <andre@itsmeij.com>
2020-05-12 11:25:22 +0200

    ...

--- db/migrate/20200430131219_make_html_processing_optional.rb
+++ db/migrate/20200430131219_make_html_processing_optional.rb
@@ -1,4 +1,4 @@
-class MakeHtmlProcessingOptional < ActiveRecord::Migration[6.0]
+class MakeHtmlProcessingOptional < ActiveRecord::Migration[5.2]
   def change
     add_column :nuntius_templates, :html_inkyrb_processing, :boolean, default: true, null: false
     add_column :nuntius_templates, :html_premailer_processing, :boolean, default: true, null: false
--- db/migrate/20200430154032_make_html_processing_optional_revert.rb
+++ db/migrate/20200430154032_make_html_processing_optional_revert.rb
@@ -1,4 +1,4 @@
-class MakeHtmlProcessingOptionalRevert < ActiveRecord::Migration[6.0]
+class MakeHtmlProcessingOptionalRevert < ActiveRecord::Migration[5.2]
   def change
     remove_column :nuntius_templates, :html_inkyrb_processing, :boolean
     remove_column :nuntius_templates, :html_premailer_processing, :boolean
