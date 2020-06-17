3430c9f25885c58b60f1924b9a8d1a66133a8a4c
Andre Meij <andre@itsmeij.com>
2020-06-17 13:02:20 +0200

    Dev mode in rails 6 and active storage :(

--- app/models/nuntius/attachment.rb
+++ app/models/nuntius/attachment.rb
@@ -3,5 +3,7 @@ module Nuntius
     has_and_belongs_to_many :messages, class_name: 'Message'
 
     delegate :download, :content_type, :filename, :signed_id, to: :content
+
+    has_one_attached :content rescue nil
   end
 end
