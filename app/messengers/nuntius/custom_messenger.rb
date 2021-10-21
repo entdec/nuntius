71fb351a910ce551add634decde1ef3735f63c0f
Mark Cornelissen <mark@boxture.com>
2021-10-21 13:41:25 +0200

    Reinstate custom templates

--- app/messengers/nuntius/custom_messenger.rb
+++ app/messengers/nuntius/custom_messenger.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 module Nuntius
-  # Custom messenger
   class CustomMessenger < BaseMessenger
-    def respond_to_missing?(event, _include_all)
-      event == @event.to_sym
-    end
-
-    def method_missing(event, object = nil, params = {})
-      super if event != @event.to_sym
-    end
   end
 end
