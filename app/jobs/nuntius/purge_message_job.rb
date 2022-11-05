b3b084a0fc0e39a77a90b61a927b78909b1364cd
Tom de Grunt <tom@degrunt.nl>
2022-11-05 13:11:39 +0100

    Don't purge bounces/complaints

--- app/jobs/nuntius/purge_message_job.rb
+++ app/jobs/nuntius/purge_message_job.rb
@@ -3,6 +3,7 @@ module Nuntius
     def perform(account_id, months)
       messages = Nuntius::Message.distinct.select(:id).where("metadata ->> 'account_id' = :account", account: account_id)
       .where(created_at: ..months.months.ago.beginning_of_day)
+      .where.not(status: %w[complaint bounced])
 
       Nuntius::Message.where(parent_message_id: messages.pluck(:id)).in_batches.update_all(parent_message_id: nil)
 
