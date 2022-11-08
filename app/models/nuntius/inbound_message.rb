79b5aa3314fbedc113167f195bc1fb9b94606c1b
Tom de Grunt <tom@degrunt.nl>
2022-11-08 12:45:36 +0100

    Missed this

--- app/models/nuntius/inbound_message.rb
+++ app/models/nuntius/inbound_message.rb
@@ -1,7 +1,7 @@
 module Nuntius
   class InboundMessage < ApplicationRecord
-    has_one_attached :raw_message, service: :nuntius
-    has_many_attached :attachments, service: :nuntius
+    has_one_attached :raw_message, service: Nuntius.config.active_storage_service
+    has_many_attached :attachments, service: Nuntius.config.active_storage_service
 
     def mail
       @mail ||= Mail.from_source(source)
