2c8e8e28408dd9a1570b3f5c1728220eba468dd3
twistedclone <d.devries@rapidlogistics.nl>
2024-09-05 09:27:56 +0200

    add comment

--- app/jobs/nuntius/purge_message_job.rb
+++ app/jobs/nuntius/purge_message_job.rb
@@ -7,6 +7,7 @@ module Nuntius
 
       Nuntius::Message.where(parent_message_id: messages.pluck(:id)).in_batches.update_all(parent_message_id: nil)
 
+      # We use a find_each now because a destroy_all gets bypassed by the `before_destroy :cleanup_attachments` method in message.rb
       messages.find_each(&:destroy)
     end
   end
