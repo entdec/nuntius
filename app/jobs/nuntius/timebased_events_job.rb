1501f65813999ba34e6fac0ed36c09ee75369141
Tom de Grunt <tom@degrunt.nl>
2024-11-11 22:02:46 +0100

    Bugfix, don't send duplicate messages

--- app/jobs/nuntius/timebased_events_job.rb
+++ app/jobs/nuntius/timebased_events_job.rb
@@ -4,10 +4,13 @@ module Nuntius
   class TimebasedEventsJob < ApplicationJob
     def perform
       Nuntius::Template.where.not(interval: nil).each do |template|
-        messenger = Nuntius::BaseMessenger.messenger_for_class(template.klass)
+        messenger_class = Nuntius::BaseMessenger.messenger_for_class(template.klass)
 
-        messenger.timebased_scope_for(template).each do |object|
-          Nuntius.event(template.event, object)
+        messenger_class.timebased_scope_for(template).each do |object|
+          messenger = messenger_class.new(object, template.event)
+          next unless messenger.is_a?(Nuntius::CustomMessenger) || messenger.respond_to?(template.event.to_sym)
+
+          messenger.dispatch([template])
         end
       end
     end
