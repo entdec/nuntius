039fe7e2da35d492f81a7a75a4ac96f0a7c1eae8
Tom de Grunt <46554+tdegrunt@users.noreply.github.com>
2026-03-27 14:16:14 +0000

    Allow subscribers to self-subscribe

--- app/controllers/nuntius/subscribers_controller.rb
+++ app/controllers/nuntius/subscribers_controller.rb
@@ -8,6 +8,37 @@ module Nuntius
     skip_before_action :verify_authenticity_token, only: :unsubscribe
     layout "empty"
 
+    def new
+      @subscriber = Nuntius::Subscriber.new(list: Nuntius::List.find(params[:list_id]))
+      render :edit
+    end
+
+    def create
+      @subscriber = Nuntius::Subscriber.new(subscriber_params)
+      if @subscriber.save
+        Signum.success(request.session.id, text: "You have been subscribed.")
+        redirect_to nuntius.edit_subscriber_path(@subscriber), status: :see_other
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def edit
+      @subscriber = Nuntius::Subscriber.find(params[:id])
+      @list = @subscriber.list
+    end
+
+    def update
+      @subscriber = Nuntius::Subscriber.find(params[:id])
+      @list = @subscriber.list
+      if @subscriber.update(subscriber_params)
+        Signum.success(request.session.id, text: "Subscription has been updated.")
+        redirect_to nuntius.edit_subscriber_path(@subscriber), status: :see_other
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
     def show
       @subscriber = Nuntius::Subscriber.find_by(id: params[:id])
       if @subscriber
@@ -31,6 +62,12 @@ module Nuntius
       @subscriber.touch(:unsubscribed_at)
       Signum.success(request.session.id, text: "Subscription has been removed.")
       redirect_to nuntius.subscriber_path(@subscriber), status: :see_other
+    end
+
+    private
+
+    def subscriber_params
+      params.require(:subscriber).permit(:email, :list_id, :first_name, :last_name, :phone_number)
     end
   end
 end
