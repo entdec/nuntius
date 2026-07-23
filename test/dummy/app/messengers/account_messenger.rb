3560c74a6d6308394adf08f7b43ff0d4fbd1a261
Tom de Grunt <tom@degrunt.nl>
2026-07-23 14:01:25 +0000

    Add test for metadata

--- test/dummy/app/messengers/account_messenger.rb
+++ test/dummy/app/messengers/account_messenger.rb
@@ -10,6 +10,18 @@ class AccountMessenger < Nuntius::BaseMessenger
   # For an after scope the time_range the interval is taken from the current time, the end of the
   # range is 1 hour from its start.
   timebased_scope :after_created do |time_range, metadata|
-    Account.where("created_at BETWEEN ? AND ?", time_range.first, time_range.last)
+    scope = Account.where("created_at BETWEEN ? AND ?", time_range.first, time_range.last)
+
+    if metadata.present?
+      if metadata["account_id"].present?
+        scope.where(id: metadata["account_id"])
+      elsif metadata["global"] == true
+        scope.all
+      else
+        scope.none
+      end
+    else
+      scope
+    end
   end
 end
--- test/jobs/nuntius/time_based_events_job_test.rb
+++ test/jobs/nuntius/time_based_events_job_test.rb
@@ -25,5 +25,24 @@ module Nuntius
       assert_equal 1, Nuntius::Message.where(template: [template1, template2]).count
       assert_includes subject.html, "- 5 days"
     end
+
+    test "uses template metadata to scope timebased templates to the account or allow globals" do
+      account = Account.create!(name: "target", created_at: 5.days.ago)
+      other_account = Account.create!(name: "other", created_at: 5.days.ago)
+
+      account_template = Nuntius::Template.create!(description: "account specific", klass: "Account", transport: "mail", event: "after_created", to: "test@example.org", html: "Hello {{name}} - account", interval: "5 days", created_at: 1.month.ago, metadata: {account_id: account.id})
+      global_template = Nuntius::Template.create!(description: "global", klass: "Account", transport: "mail", event: "after_created", to: "test@example.org", html: "Hello {{name}} - global", interval: "5 days", created_at: 1.month.ago, metadata: {global: true})
+      other_account_template = Nuntius::Template.create!(description: "other account", klass: "Account", transport: "mail", event: "after_created", to: "test@example.org", html: "Hello {{name}} - other", interval: "5 days", created_at: 1.month.ago, metadata: {account_id: other_account.id})
+
+      assert_difference -> { Nuntius::Message.count }, 4 do
+        perform_enqueued_jobs only: [Nuntius::MessengerJob, Nuntius::TimebasedEventsJob] do
+          Nuntius::TimebasedEventsJob.perform_now
+        end
+      end
+
+      assert_equal [account_template.id, global_template.id].sort, Nuntius::Message.where(nuntiable: account).pluck(:template_id).uniq.sort
+      assert_equal [global_template.id, other_account_template.id].sort, Nuntius::Message.where(nuntiable: other_account).pluck(:template_id).uniq.sort
+      assert_equal 0, Nuntius::Message.where(template: other_account_template).where(nuntiable: account).count
+    end
   end
 end
